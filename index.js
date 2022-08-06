6d0d9739b325f72a60933b062bfa59e49dc8cde1
VishwajeetSinghParihar750 <sVishwajeet750@gmail.com>
2022-08-06 20:43:34 +0530

    reapplies cors

--- index.js
+++ index.js
@@ -20,7 +20,12 @@ const app = express();
 // middlewares
 app.use(cookieParser());
 
-// app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({
+    origin: "https://cookie-auth-project-client.vercel.app/",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(morgan("dev")); // gives data about incoming requests on console
